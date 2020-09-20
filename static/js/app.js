d205faef2d006a4f93ea74f7f650033966b3758e
arhunt <allan.r.hunt@gmail.com>
2020-09-20 15:36:31 -0500

    Insert conditional statement.

--- static/js/app.js
+++ static/js/app.js
@@ -8,6 +8,7 @@ var tbody = d3.select("tbody")
 
 // Create event handlers
 filters.on("submit", runEnter);
+// Filters is not working anymore??
 button.on("click", runEnter);
 
 // Event handler
@@ -16,18 +17,41 @@ function runEnter() {
     // Prevent the page from refreshing
     d3.event.preventDefault();
 
+    // Grab date
     var inputDateElement = d3.select("#datetime");
-
     var inputDate = inputDateElement.property("value");
 
+    // Grab city
+    var inputCityElement = d3.select("#city");
+    var inputCity = inputCityElement.property("value");
+
+    // Grab state
+    var inputStateElement = d3.select("#state");
+    var inputState = inputStateElement.property("value");
+
+    // Grab country
+    var inputCountryElement = d3.select("#country");
+    var inputCountry = inputCountryElement.property("value");
+
+    // Grab shape
+    var inputShapeElement = d3.select("#shape");
+    var inputShape = inputShapeElement.property("value");
+
     console.log(inputDate);
-    console.log(tableData);
+    console.log(inputCity);
+    console.log(inputState);
+    console.log(inputCountry);
+    console.log(inputShape);
 
     // idea to start with filteredData = tableData and construct IFs for each filter
-    // var filteredData = tableData;
+    var filteredData = tableData;
 
-    // if filteredData != "";
-    var filteredData = tableData.filter(datum => datum.datetime === inputDate);
+    if (inputDate != "") {
+        var filteredData = filteredData.filter(datum => datum.datetime === inputDate);
+    }
+    else {
+        var filteredData = filteredData
+    }
 
     // else filteredData = filteredData
     console.log(filteredData);
