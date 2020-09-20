bc66d21a77c9c8e77c18409c8c7b38357ad3ec7e
arhunt <allan.r.hunt@gmail.com>
2020-09-20 14:51:26 -0500

    Construct filter.

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,32 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Select action areas of the page
+var filters = d3.select(".filters");
+var button = d3.select("#filter-btn");
+
+// Create event handlers
+filters.on("submit", runEnter);
+button.on("click", runEnter);
+
+// Event handler
+function runEnter() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    var inputDateElement = d3.select("#datetime");
+
+    var inputDate = inputDateElement.property("value");
+
+    console.log(inputDate);
+    console.log(tableData);
+
+    var filteredData = tableData.filter(datum => datum.datetime === inputDate);
+
+    console.log(filteredData);
+
+    // Clear the current data
+    //list.html("");
+
+};
