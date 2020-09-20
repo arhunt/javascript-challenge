248cee6e709527d5c9e01d761e4d04f7f8318482
arhunt <allan.r.hunt@gmail.com>
2020-09-20 15:05:49 -0500

    Generate table.

--- static/js/app.js
+++ static/js/app.js
@@ -4,6 +4,7 @@ var tableData = data;
 // Select action areas of the page
 var filters = d3.select(".filters");
 var button = d3.select("#filter-btn");
+var tbody = d3.select("tbody")
 
 // Create event handlers
 filters.on("submit", runEnter);
@@ -22,11 +23,24 @@ function runEnter() {
     console.log(inputDate);
     console.log(tableData);
 
+    // idea to start with filteredData = tableData and construct IFs for each filter
+    // var filteredData = tableData;
+
+    // if filteredData != "";
     var filteredData = tableData.filter(datum => datum.datetime === inputDate);
 
+    // else filteredData = filteredData
     console.log(filteredData);
 
     // Clear the current data
-    //list.html("");
+    tbody.html("");
+
+    filteredData.forEach(function (datum) {
+        var row = tbody.append("tr");
+        Object.entries(datum).forEach(function([key, value]) {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
 
 };
